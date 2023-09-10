dd507280dc251ea8772c41dd81fd5da2d90294fe
eah645 <92736056+eah645@users.noreply.github.com>
2023-09-09 20:08:16 -0400

    Rebuilt alumni page
    This came out so much better than the first time around

--- script.js
+++ script.js
@@ -5,21 +5,44 @@
 //Oh my god fix text- make text responsive or the fucking cards need to STACK PLEASE-
 // ^ Use @media. Anything smaller than like- 1000px make the cards 100vw
 
+//Function to change class after being selected
+//So... I think you're gonna need to update this every year to account for the new class because rn idk how to do it for all of them
+function titleChange() {
+  //Clear all other ones, rn just gonna test with a class of 2023 / 2024 / 2025
+}
+
+//Class of 2023 vue
 const vue_app_2023 = Vue.createApp({
-    created() {
-      fetch("JSON/2023.json")
-        .then((response) => response.json())
-        .then((json) => {
-          this.First = json;
-        });
-    },
-    data() {
-      return {
-        First: [],
-        title: "Class of 2023",
-      };
-    }
+     created() {
+       fetch("JSON/2023.json")
+         .then((response) => response.json())
+         .then((json) => {
+           this.First = json;
+         });
+     },
+     data() {
+       return {
+         First: [],
+         title: "Class of 2023",
+       };
+     }
+   });
+  
+   vue_app_2023.mount("#vue_app_2023");
+
+
+//Credit: https://codepen.io/kianoush/pen/GRpwZJy
+  const navbarButton = document.getElementById("showInfos");
+  const activityInfo = document.querySelector(".activity-info");
+  
+  navbarButton.addEventListener("click", (e) => {
+    activityInfo.classList.toggle("showNavbar");
+    console.log("Working")
   });
   
-  vue_app_2023.mount("#vue_app_2023");
+  activityInfo.addEventListener("click", (e) => {
+    if (e.target.classList.contains("activity-info")) {
+      activityInfo.classList.remove("showNavbar");
+    }
+  });
   
